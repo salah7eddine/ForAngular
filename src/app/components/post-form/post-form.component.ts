c37e17e6aefcdd18c983762333521335e778586c
ABOUSAID <salah-eddine.abousaid@capgemini.com>
2019-07-06 10:38:33 +0100

    Input & Edit State

--- src/app/components/post-form/post-form.component.ts
+++ src/app/components/post-form/post-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { PostService } from './../../services/post.service';
 import { Post } from './../../models/Post';
 
@@ -8,6 +8,8 @@ import { Post } from './../../models/Post';
   styleUrls: ['./post-form.component.css']
 })
 export class PostFormComponent implements OnInit {
+  post: Post;
+  @Output() newPost: EventEmitter<Post> = new EventEmitter();
 
   constructor(private postService: PostService) { }
 
@@ -19,7 +21,7 @@ export class PostFormComponent implements OnInit {
       alert('Please add post');
     } else {
       this.postService.savePost({ title, body } as Post).subscribe(post => {
-        console.log(post);
+        this.newPost.emit(post);
       });
     }
   }
