cce57cb8346130d98a28aa869f2f2eca887a4bfb
ABOUSAID <salah-eddine.abousaid@capgemini.com>
2019-07-06 10:10:00 +0100

    create Posts & work with HTTP Client Setup & GET Request

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { User } from './../../models/User';
 import { DataService } from './../../services/data.service';
+import { UserService } from './../../services/user.service';
 
 @Component({
   selector: 'app-users',
@@ -21,18 +22,27 @@ export class UsersComponent implements OnInit {
   @ViewChild('userForm', { static: false }) form: any;
   data: any;
 
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService, private userService: UserService) { }
 
   ngOnInit() {
 
-    this.dataService.getData().subscribe(data => {
+    this.userService.getData().subscribe(data => {
       console.log(data);
     });
 
-    this.dataService.getUsers().subscribe(users => {
+    this.userService.getUsers().subscribe(users => {
       this.users = users;
       this.loaded = true;
     });
+
+    // this.dataService.getData().subscribe(data => {
+    //   console.log(data);
+    // });
+
+    // this.dataService.getUsers().subscribe(users => {
+    //   this.users = users;
+    //   this.loaded = true;
+    // });
 
 
   }
@@ -64,7 +74,8 @@ export class UsersComponent implements OnInit {
       value.registered = new Date();
       value.hide = true;
 
-      this.dataService.addUser(value);
+      this.userService.addUser(value);
+      // this.dataService.addUser(value);
       this.form.reset();
     }
   }
